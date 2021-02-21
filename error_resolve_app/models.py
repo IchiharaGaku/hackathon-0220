9332c4469cb10873a22226d9dbaf64768789a655
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-02-21 11:24:09 +0900

    Modify template

--- error_resolve_app/models.py
+++ error_resolve_app/models.py
@@ -8,4 +8,9 @@ class Article(models.Model):
     movie_name = models.CharField(max_length=200, default="")
     thumb_frame = models.IntegerField(default=0)
     upload_file_name = models.CharField(max_length=200, default="")
+    content = models.TextField(blank=True, null=True)
+    title = models.CharField(max_length=100, null=True)
+    created_at = models.DateTimeField(default=timezone.now, blank=True, null=True)
+
+
     
